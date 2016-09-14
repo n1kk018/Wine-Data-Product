311868b0810459438e4b4f7d70c0fa9430ebdc31
ronan <ronan@RL-Satellite-P850>
2016-09-14 14:21:25 +0200

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/fr/afcepf/atod/mbeans/mbeanorder/MBeanCartManagement.java
    	src/main/java/fr/afcepf/atod/mbeans/mbeanproduct/MBeanProduct.java
    	src/main/webapp/WEB-INF/templates/commons.xhtml
    	src/main/webapp/index.xhtml
    	src/main/webapp/pages/basket.xhtml

--- src/main/java/fr/afcepf/atod/wine/data/product/api/IDaoProduct.java
+++ src/main/java/fr/afcepf/atod/wine/data/product/api/IDaoProduct.java
@@ -1,10 +1,24 @@
 package fr.afcepf.atod.wine.data.product.api;
 
 
+import fr.afcepf.atod.vin.data.exception.WineException;
 import fr.afcepf.atod.wine.data.api.IDaoGeneric;
 import fr.afcepf.atod.wine.entity.Product;
+import java.util.List;
 
 public interface IDaoProduct extends IDaoGeneric<Product, Integer> {
-	
-	Product findByName(String name);
+	/**
+         * 
+         * @param name
+         * @return
+         * @throws WineException 
+         */
+	Product findByName(String name) throws WineException;
+        /**
+         * 
+         * @param min
+         * @return
+         * @throws WineException 
+         */
+        List<Product> findExpensiveProducts(double min) throws WineException;
 }
--- src/main/java/fr/afcepf/atod/wine/data/product/impl/DaoProduct.java
+++ src/main/java/fr/afcepf/atod/wine/data/product/impl/DaoProduct.java
@@ -1,5 +1,7 @@
 package fr.afcepf.atod.wine.data.product.impl;
 
+import fr.afcepf.atod.vin.data.exception.WineErrorCode;
+import fr.afcepf.atod.vin.data.exception.WineException;
 import javax.management.Query;
 import javax.transaction.Transactional;
 
@@ -10,35 +12,65 @@ import org.springframework.stereotype.Service;
 import fr.afcepf.atod.wine.data.impl.DaoGeneric;
 import fr.afcepf.atod.wine.data.product.api.IDaoProduct;
 import fr.afcepf.atod.wine.entity.Product;
-
+import java.util.List;
 
 @Service
 @Transactional
-public class DaoProduct extends DaoGeneric<Product, Integer> implements IDaoProduct{
-	 /*****************************************************.
+public class DaoProduct extends DaoGeneric<Product, Integer> implements IDaoProduct {
+
+    /******************************************************
      *                  Requetes HQL
-     ****************************************************/
+     * **************************************************  */
+    private static final String REQFINDBYNAME = "SELECT p FROM Product p "
+            + "WHERE p.name = :name";
+    private static final String REQEXPPROD = "SELECT p FROM "
+            + "Product p WHERE p.price > :paramMin";
 
-    	private static final String REQFINDBYNAME = "SELECT p FROM Product p "
-                + "WHERE p.name = :name";
+    /**
+     * **************************************************. Fin Requetes HQL
+     * **************************************************
+     */
+    /**
+     *
+     * @param name
+     * @return
+     */
+    @Override
+    public Product findByName(String name) throws WineException {
+        Product p = null;
+        if (!name.equals("")) {
+            p = (Product) (getSf().getCurrentSession()
+                    .createQuery(REQFINDBYNAME)
+                    .setParameter("name", name)
+                    .uniqueResult());
+            if (p == null) {
+                throw new WineException(WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+                        "the product named " + name + " has not been"
+                        + " found in the database.");
+            }
+        } else {
+            throw new WineException(WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+                    "the product named " + name + " has not been"
+                    + " found in the database.");
+        }
 
-    /****************************************************.
-     *                 Fin Requetes HQL
-     ****************************************************/
-	
-	/**
-         * 
-         * @param name
-         * @return 
-         */
-	@Override
-	public Product findByName(String name) {
-		Product p = null;
-		p = (Product)(getSf().getCurrentSession()
-			.createQuery(REQFINDBYNAME)
-			.setParameter("name", name)
-			.uniqueResult());
-		
-		return p;
-	}
+        return p;
+    }
+
+    @Override
+    public List<Product> findExpensiveProducts(double min) throws WineException {
+        List<Product> expensiveProds = null;
+        if (min >= 0) {
+            expensiveProds = getSf().getCurrentSession().createQuery(REQEXPPROD)
+                    .setParameter("paramMin", min).list();
+            if (expensiveProds.isEmpty()){
+                throw new WineException(WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+                    "Criteria not found in the database");
+            }
+        } else {
+            throw new WineException(WineErrorCode.RECHERCHE_NON_PRESENTE_EN_BASE,
+                    "Wrong criteria");
+        }
+        return expensiveProds;
+    }
 }
